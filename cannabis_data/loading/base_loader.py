216c270586fea0345fda2b06f0780a4adb2b31ae
Sina <stavakkoli1999@gmail.com>
2025-12-07 05:48:08 +0330

    FIX: refactoring

--- cannabis_data/loading/base_loader.py
+++ cannabis_data/loading/base_loader.py
@@ -2,7 +2,6 @@ from abc import ABC, abstractmethod
 import pandas as pd
 
 class BaseLoader(ABC):
-    """Abstract loader that returns a pandas DataFrame."""
 
     @abstractmethod
     def load(self) -> pd.DataFrame:
